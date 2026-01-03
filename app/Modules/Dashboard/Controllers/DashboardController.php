531b630e70077287744c556f031f4487a5f796bb
Long <longshare9201@gmail.com>
2026-01-03 11:06:38 +0700

    fix dashboard

--- app/Modules/Dashboard/Controllers/DashboardController.php
+++ app/Modules/Dashboard/Controllers/DashboardController.php
@@ -3,12 +3,22 @@
 namespace App\Modules\Dashboard\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Modules\Dashboard\Services\DashboardService;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    protected $dashboardService;
+
+    public function __construct(DashboardService $dashboardService)
+    {
+        $this->dashboardService = $dashboardService;
+    }
+
     public function index()
     {
-        return view('dashboard::index');
+        $data = $this->dashboardService->getDashboardData();
+
+        return view('dashboard::index', $data);
     }
 }
